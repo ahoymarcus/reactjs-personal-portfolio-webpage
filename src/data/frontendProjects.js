fc55984ce418feb74a87f5ee55689449e7bcaf88
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2022-01-01 16:36:27 -0300

    Corrigir dados projetos Frontend

--- src/data/frontendProjects.js
+++ src/data/frontendProjects.js
@@ -16,12 +16,12 @@ export let frontendProjects = [
 		tags: ['React-JS', 'Redux', 'React-redux', 'React-Router-Dom', 'styled-components', 'API REST', 'Axios'],
 	},
 	{
-		title: 'Redux Toolkit Cinema',
+		title: 'Redux Cinema App',
 		urlRepository: 'https://github.com/ahoymarcus/redux-toolkit-reactjs-cinema',
 		urlApp: 'https://redux-toolkit-reactjs-cinema.netlify.app/',
 		image: '/images/frontend/redux-toolkit-reactjs-cinema-01.png',
 		description: 'App React-JS de filmes-sériees. Como funcionalidades especiais usa o React-router-dom para roteamento no frontend e o Reduxjs-toolkit para simplificar o uso do Redux ao gerenciar o estado geral da aplicação.',
-		tags: ['React-JS', 'Redux', 'React-redux', 'Reduxjs-Toolkit', 'React-Router-Dom', 'API REST', 'Axios', 'Node-SCSS'],
+		tags: ['React-JS', 'Redux', 'Reduxjs-Toolkit', 'React-Router-Dom', 'API REST', 'Axios', 'Node-SCSS'],
 	},
 	{
 		title: 'Shopping Cart App',
--- src/data/projects-data-schema.js
+++ src/data/projects-data-schema.js
@@ -1,11 +1,11 @@
 export let projectsDataSchema = [
 	{
-		title: 'Redux-Toolkit Cinema App',
-		urlRepository: 'https://github.com/ahoymarcus/redux-toolkit-reactjs-cinema',
-		urlApp: 'https://redux-toolkit-reactjs-cinema.netlify.app/',
-		image: '/images/frontend/redux-toolkit-reactjs-cinema-01.png',
-		description: 'App React-JS de filmes-sériees. Como funcionalidades especiais usa o React-router-dom para roteamento no frontend e o Reduxjs-toolkit para simplificar o uso do Redux ao gerenciar o estado geral da aplicação.',
-		tags: ['React-JS', 'Redux', 'React-redux', 'Reduxjs--Toolkit', 'React-Router-Dom', 'API REST', 'Axios', 'Node-SCSS', 'React-Slick'],
+		title: '',
+		urlRepository: '',
+		urlApp: '',
+		image: '/images//',
+		description: '',
+		tags: ['React-JS', 'Redux', 'Reduxjs-Toolkit', 'React-Router-Dom', 'API REST', 'Axios', 'Node-SCSS', 'React-Slick'],
 	},
 ];
 
